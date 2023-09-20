61df6518cd5460b507a873f1413aea83b31a6bdc
Sebastian Jurewicz <senkartan@gmail.com>
2023-09-20 11:32:03 +0200

    Navigation and hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./index.html'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Montserrat', 'sans-serif'],
+			},
+		},
+		container: {
+			center: true,
+		},
+	},
+	plugins: [],
+};
